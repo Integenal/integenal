b54953f02f5f295d8fb6b6777651222122315d49
Integenal <integenal@gmail.com>
2024-04-18 10:17:18 +0300

    ghjk

--- src/app/component/header.tsx
+++ src/app/component/header.tsx
@@ -1,32 +1,40 @@
 "use client";
 import React, { useEffect, useState, useRef, useCallback } from "react";
-import { ScrollCont } from "../methods/scroll";
 import { Anim } from "../methods/anim";
 import Image from "next/image";
 const Header: React.FC = () => {
+  //  PART 1
+  //variables are used to track the state of the background image loading
+  // the height of the container element, the vertical scroll position, and the scroll progress,
   const [imageLoaded, setImageLoaded] = useState(false);
-  const refContainer = useRef<HTMLDivElement>(null);
+  const refContainer = useRef<HTMLDivElement>(null); //it will access the container <div> element in the JSX code
   const [containerHeight, setContainerHeight] = useState(0);
   const [scrollY, setScrollY] = useState(0);
   const [progress, setProgress] = useState(0);
 
+  //  PART 2
   const handleImageLoaded = useCallback(() => {
-    setImageLoaded(true);
-  }, []);
+    setImageLoaded(true); //sets the imageLoaded state variable to true when called
+  }, []); //doesn't depend on any external values, its memoized
+
   React.useEffect(() => {
     handleImageLoaded();
   }, [handleImageLoaded]);
 
+  //responsible for handling resize and scroll events
   useEffect(() => {
+    //function 1
     const handleResize = () => {
       if (refContainer.current) {
         setContainerHeight(refContainer.current.clientHeight);
       }
     };
 
+    //function 2
     const handleScroll = () => {
       setScrollY(window.scrollY);
     };
+    //function 1 and 2 are called immediately after their definitions to measure the initial dimensions and positions
 
     handleResize(); // Measure container height initially
     handleScroll(); // Measure scroll position initially
@@ -40,17 +48,18 @@ const Header: React.FC = () => {
     };
   }, []);
 
+  //calculates the scroll progress based on the scrollY and containerHeigh
   useEffect(() => {
     if (containerHeight > 0) {
-      setProgress(Math.min(1, scrollY / containerHeight));
+      setProgress(Math.min(1, scrollY / containerHeight)); //re-render the component to reflect the new scroll progress visually.
     }
   }, [scrollY, containerHeight]);
+
   return (
     <div
       ref={refContainer}
       className="min-h-screen p-12 flex flex-col items-center justify-center sticky top-0 -z-10"
       style={{ transform: `translateY(-${progress * 20}vh)` }}>
-      {" "}
       <video
         autoPlay
         loop
@@ -74,7 +83,6 @@ const Header: React.FC = () => {
       <div className="p-12 z-10 text-white drop-shadow-[0_5px_3px_rgba(0,0,0,0.6)] text-center flex flex-1 justify-center items-center flex-col">
         <Anim initialX={0} initialY={0} visibleX={0} visibleY={0}>
           <>
-            {" "}
             <h1 className="mb-6 font-Popp2 text-4xl xl:text-5xl">Integenal</h1>
             <h2 className="mb-2 text-xl xl:text-2xl tracking-tight">
               <span>Web Development, </span>
@@ -98,49 +106,3 @@ const Header: React.FC = () => {
   );
 };
 export default Header;
-
-/*<Avatar
-  avatarStyle='Transparent'
-  topType='ShortHairShortCurly'
-  accessoriesType='Blank'
-  hairColor='Black'
-  facialHairType='MoustacheFancy'
-  facialHairColor='Black'
-  clotheType='Hoodie'
-  clotheColor='Gray02'
-  eyeType='Surprised'
-  eyebrowType='Default'
-  mouthType='Serious'
-  skinColor='Light'
-
-
-  <Avatar
-  avatarStyle='Transparent'
-  topType='ShortHairShortWaved'
-  accessoriesType='Blank'
-  hairColor='Black'
-  facialHairType='BeardLight'
-  facialHairColor='Black'
-  clotheType='ShirtVNeck'
-  clotheColor='Black'
-  eyeType='Squint'
-  eyebrowType='DefaultNatural'
-  mouthType='Default'
-  skinColor='Light'
-/>
-
-
-<Avatar
-  avatarStyle='Transparent'
-  topType='ShortHairShortFlat'
-  accessoriesType='Blank'
-  hairColor='Brown'
-  facialHairType='Blank'
-  clotheType='ShirtCrewNeck'
-  clotheColor='PastelBlue'
-  eyeType='Happy'
-  eyebrowType='DefaultNatural'
-  mouthType='Smile'
-  skinColor='Light'
-/>
-/>*/
--- src/app/methods/anim.tsx
+++ src/app/methods/anim.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useRef, useEffect } from "react";
-import { motion, inView, useAnimation, useInView } from "framer-motion";
+import { motion, useAnimation, useInView } from "framer-motion";
 
 interface Props {
   children: JSX.Element;
@@ -19,17 +19,23 @@ export const Anim = ({
   children,
   width,
 }: Props) => {
-  const ref = useRef(null);
-  const controls = useAnimation();
-  const isInView = useInView(ref, { once: true });
+  const ref = useRef(null); //A reference to the DOM element that the animation will be applied to.
+  const controls = useAnimation(); //useAnimation() hook from Framer Motion to control animations.
+  const isInView = useInView(ref, {
+    once: true,
+  }); //the component is currently in the viewport
+  //{once: true,}  ensures that animation occurs only once when the component comes into view
+
   useEffect(() => {
     if (isInView) {
-      controls.start("visible");
+      //gets triggered if the val of inv changes
+      controls.start("visible"); //starts the animation by calling
     }
   }, [controls, isInView]);
+
   const childrenArray = Array.isArray(children) ? children : [children];
   return (
-    <div ref={ref} className={``} style={{ width }}>
+    <div ref={ref} style={{ width }}>
       {childrenArray.map((child, index) => (
         <motion.div
           key={index}
